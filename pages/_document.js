18de197b4a9c70d57b9cfce533963fbc5a711cb6
jemnon <davidjosepharias@gmail.com>
2019-04-20 00:00:03 -0700

    setting up document

---
+++ pages/_document.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css?family=Lato|Noto+Serif+TC"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
