93f6e80f43e757f8eb4edbe81c9b6f8b6cefc647
jemnon <davidjosepharias@gmail.com>
2020-04-11 22:31:35 -0700

    fixed broken favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -91,7 +91,7 @@ module.exports = {
         background_color: `#f4ede6`,
         theme_color: `#cf7651`,
         display: `minimal-ui`,
-        icon: `src/images/favicon.ico`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
