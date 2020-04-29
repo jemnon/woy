045160e729cc04eeb286bf4400a4504ccc625eb6
jemnon <davidjosepharias@gmail.com>
2020-04-28 21:42:05 -0700

    es5 syntax

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,7 @@
 var detectBrowser = require('detect-browser');
 var browser = detectBrowser.detect();
 
-exports.onClientEntry = () => {
+exports.onClientEntry = function() {
   if (browser.name === 'ie') {
     window.location = 'https://browsehappy.com/';
   }
