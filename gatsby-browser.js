ba9c6c94f6d2139a3a4bee6da854244d0c36feb2
jemnon <davidjosepharias@gmail.com>
2020-04-28 21:06:34 -0700

    added logging for browser detect

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,6 +2,7 @@ const { detect } = require('detect-browser');
 const browser = detect();
 
 exports.onClientEntry = () => {
+  console.log('browser name: ', browser.name);
   if (browser.name === 'ie') {
     window.location.href = 'https://browsehappy.com/';
   }
