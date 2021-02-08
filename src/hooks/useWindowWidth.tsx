32db20a2615c3e68c932de370a080f79866cdf93
jemnon <davidjosepharias@gmail.com>
2021-02-07 20:30:01 -0800

    fix ssr

--- src/hooks/useWindowWidth.tsx
+++ src/hooks/useWindowWidth.tsx
@@ -2,7 +2,8 @@ import { useEffect, useState } from 'react';
 import useDebounce from './useDebounce';
 
 const useWindowWidth = (onResize: (width: number | string) => void): void => {
-  const [width, setWidth] = useState<number>(window?.innerWidth || 0);
+  const initialWidth = typeof window !== `undefined` ? window.innerWidth : 0;
+  const [width, setWidth] = useState<number>(initialWidth);
   const debouncedWidth = useDebounce(width);
   useEffect(() => {
     const handleResize = (): void => {
