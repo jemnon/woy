f4b42f442cd2203365522a2f0f53a6c5bcca2365
jemnon <davidjosepharias@gmail.com>
2021-02-17 23:34:58 -0800

    show more

--- src/molecules/ShowMore/stories/ShowMore.stories.tsx
+++ src/molecules/ShowMore/stories/ShowMore.stories.tsx
@@ -2,30 +2,13 @@ import React from 'react';
 import { Meta } from '@storybook/react';
 import ShowMore from '../ShowMore';
 
-const disableProp = {
-  table: {
-    disable: true,
-  },
-};
-
 export default {
-  title: 'FILL IN/ShowMore',
-  component: ShowMore, 
-  argTypes: {
-    as: { ...disableProp },
-    ref: { ...disableProp },
-    theme: { ...disableProp },
-    forwardedAs: {
-      ...disableProp,
-    },
-  },
+  title: 'Molecules/ShowMore',
+  component: ShowMore,
 } as Meta;
 
-const Template = (args: any) => (
-  <ShowMore {...args} />
-);
+const Template = (args: any) => <ShowMore {...args} />;
 
 export const Default = Template.bind({});
 // @ts-ignore
 Default.args = {};
-
